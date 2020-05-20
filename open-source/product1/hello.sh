7173fec6f6676f0420c893db9a2c444e46db4952
jonahjon <jonahjones094@gmail.com>
2020-05-20 13:19:29 -0400

    looking at kubeconfig

--- open-source/product1/hello.sh
+++ open-source/product1/hello.sh
@@ -2,4 +2,12 @@
 
 echo $KUBECONFIG
 
-export KUBECONFIG=$KUBECONFIG && cd ../../go && go test -timeout=0 -v -ginkgo.v ./e2e_test.go
+cat $KUBECONFIG
+
+cd ../../go
+
+ls -la
+
+go test -timeout=0 -v -ginkgo.v ./e2e_test.go
+
+# export KUBECONFIG=$KUBECONFIG && cd ../../go && go test -timeout=0 -v -ginkgo.v ./e2e_test.go
