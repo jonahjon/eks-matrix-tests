dd5ec1ece18c1a7bb75c90aafeedd459ddf96a20
jonahjon <jonahjones094@gmail.com>
2020-06-12 17:45:36 -0400

    spyglass gcs

--- images/publish-image.sh
+++ images/publish-image.sh
@@ -7,6 +7,37 @@ usage () {
     exit 1
 }
 
+function start_docker() {
+    echo "Docker in Docker enabled, initializing..."
+    printf '=%.0s' {1..80}; echo
+    # If we have opted in to docker in docker, start the docker daemon,
+    service docker start
+    # the service can be started but the docker socket not ready, wait for ready
+    local WAIT_N=0
+    local MAX_WAIT=20
+    while true; do
+        # docker ps -q should only work if the daemon is ready
+        docker ps -q > /dev/null 2>&1 && break
+        if [[ ${WAIT_N} -lt ${MAX_WAIT} ]]; then
+            WAIT_N=$((WAIT_N+1))
+            echo "Waiting for docker to be ready, sleeping for ${WAIT_N} seconds."
+            sleep ${WAIT_N}
+        else
+            echo "Reached maximum attempts, not waiting any longer..."
+            exit 1
+        fi
+    done
+    printf '=%.0s' {1..80}; echo
+
+    docker-credential-gcr configure-docker
+    echo "Done setting up docker in docker."
+}
+
+
+start_docker
+
+export $(aws ecr get-login-password --region us-west-2) 
+
 readonly SOURCES_DIR=$1
 
 if [[ -z "${SOURCES_DIR}" ]]; then
