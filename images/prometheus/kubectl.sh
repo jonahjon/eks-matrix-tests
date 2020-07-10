6771a8b34d9ee8158721e1b91e2593503f88f432
jonahjon <jonahjones094@gmail.com>
2020-07-10 14:26:11 -0400

    adding in prometheus

---
+++ images/prometheus/kubectl.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+DISABLE_MD_LINTING=1
+DISABLE_MD_LINK_CHECK=1
+export GO111MODULE=on
+
+#in the prowjob  (1_14 | 1_15 | 1_16)
+export KUBECONFIG=/workspace/"$@"_cluster.config
+
+echo "******************************************************"
+echo "installing kubectl manifest for Prometheus"
+echo "******************************************************"
+
+kubectl apply -f images/prometheus/templates/prometheus.yaml --wait
+
+echo "******************************************************"
+echo "checking logs"
+echo "******************************************************"
+
+kubectl logs -l app.kubernetes.io/instance=prometheus
+
+echo "******************************************************"
+echo "Running Tests"
+echo "******************************************************"
+
+kubectl apply -f images/prometheus/templates/tests/tests.yaml --wait
+
+kubectl get pod -l app.kubernetes.io/name=prometheus-kubectl-test --field-selector=status.phase!=Running
+
+echo "******************************************************"
+echo "Tests Passed ........ deleting Grafana"
+echo "******************************************************"
+
+kubectl delete -f images/prometheus/templates/tests/tests.yaml --wait
+
+kubectl delete -f images/prometheus/templates/prometheus.yaml --wait
+
+echo "******************************************************"
+echo "Prowjob Finished Sucessfully"
+echo "******************************************************"
